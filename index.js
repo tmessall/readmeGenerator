10aea3604a5b50d87bdc35a1464c8d54a8f731b9
tmessall <tmessall17@gmail.com>
2020-10-16 16:45:47 -0600

    Completed the generate markdown function and called in index.js

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // array of questions for user
 const questions = [
     {
+        // inquirer defaults to input for type
         message: "What is the project title?",
-        name: "project"
+        name: "title"
     },
     {
         message: "What is the project description?",
         name: "description"
     },
     {
-        message: "What are the project installation instructions?",
+        message: "What are the installation instructions?",
         name: "installation"
     },
     {
-        message: "What are the project usage instructions?",
+        message: "What are the usage instructions?",
         name: "usage"
     },
     {
-        message: "What are the project test instructions?",
+        message: "What are the contribution guidelines?",
+        name: "contributing"
+    },
+    {
+        message: "What are the test instructions?",
         name: "testIns"
     },
     {
         type: "list",
         message: "Select a license:",
-        choices: ["ex1", "ex2"],
+        choices: ["ISC", "MIT", "BSD3", "Apache 2.0"],
         name: "license"
     },
     {
@@ -41,17 +47,17 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+    console.log(data);
 }
 
 // function to initialize program
 function init() {
     inquirer.prompt(questions).then(function (response) {
         try {
-            console.log(response);
+            writeToFile("example.md", generateMarkdown(response));
         } catch (error) {
             console.log("error");
         }
-        
     })
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,38 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+![Shield](https://img.shields.io/badge/license-${data.license}-blue)
+# ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+[Installation](#installation)
+[Usage](#usage)
+[License](#license)
+[Contributing](#contributing)
+[Tests](#tests)
+[Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This application is covered under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.testIns}
+
+## Questions
+<a href="https://github.com/${data.ghUser}">Github</a>
+Email me at: ${data.email} with any further questions.
 `;
 }
 
